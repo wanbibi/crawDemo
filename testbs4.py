508be994accce4854fafd72a21999e1a12175768
Administrator <593128836@qq.com>
2016-11-01 21:17:07 +0800

    python demo

---
+++ testbs4.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+html_doc='''
+<html>
+
+<head>
+<title>我的第一个 HTML 页面</title>
+</head>
+
+<body>
+<p>body 元素的内容会显示在浏览器中。</p>
+<p>title 元素的内容会显示在浏览器的标题栏中。</p>
+</body>
+
+</html>
+'''
+soup = BeautifulSoup(html_doc,'html.parser',from_encoding='utf8')
+
+p_node = soup.find('head')
+print p_node.get_text(), p_node.name
